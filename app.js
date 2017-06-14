5ad7daf8e8b73bb9de40a0078725f8b183906714
Eduardo Vedes <eduardo.vedes@gmail.com>
2017-06-14 17:23:11 +0100

    update

--- app.js
+++ app.js
@@ -3,7 +3,19 @@ const app = express()
 const path = require('path')
 const port = process.env.PORT || 8080
 
+
+
 app.use('/', express.static(path.join(__dirname, 'public')))
+
+app.get('/api', (req,res)=>{
+    res.json({
+        "IP address" : req.headers['x-forwarded-for'],
+        "Language" : req.headers['accept-language'],
+        "Browser": req.headers['user-agent']
+    })
+        
+})
+
 
 
 app.listen(port, ()=>{
